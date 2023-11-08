73617547bde9acddd2e00229327f0315c08b1efd
Vedansh Jain <vedansh.code@gmail.com>
2023-11-08 19:40:17 +0530

    More Clubs And Cards Added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Devoiclogo from '../assets/Devoiclogo.png'
-import codingclub from '../assets/codingclub.jpg'
+import codingclublogo from '../assets/code4.jpg'
+import code4alllogo from '../assets/code4all3.png'
 
 const Cards = () => {
   return (
     <>
       <section class="text-white px-12 cards-section">
         <h1 className="text-center text-5xl">Technical Clubs</h1>
-        <div class="container px-5 py-12 mx-auto">
+        <div class="container px-5 pt-12 mx-auto">
           <div class="flex flex-wrap -m-4">
             <div class="xl:w-1/4 md:w-1/2 p-4">
               <div class="club-cards p-6">
@@ -86,6 +87,106 @@ const Cards = () => {
                 <img
                   class="h-20 rounded w-full object-contain object-center mb-6"
                   src="https://logogen.gdscasu.com/logos/gdsc-logo.png"
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-xs font-medium title-font">
+                  Help Learn Connect
+                </h3>
+                <h2 class="text-lg  font-medium title-font mb-4">
+                  Team GDSC
+                </h2>
+                <p class="leading-relaxed text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, mollitia.
+                </p>
+                <div className="flex gap-2 mt-4 social-links w-fit rounded-lg px-2">
+                    <h1 className="text-2xl"><a href="https://www.instagram.com/gdsc_uitrgpv/" target="_blank"><i class="ri-instagram-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://www.linkedin.com/company/gdscuitrgpv/" target="_blank"><i class="ri-linkedin-fill"></i></a></h1>
+                    <h1 className="text-2xl"><a href="" target="_blank"><i class="ri-twitter-x-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://gdscuitrgpv.vercel.app/" target="_blank"><i class="ri-links-line"></i></a></h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container px-5 pb-12 pt-0 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+              <div class="club-cards p-6">
+                <img
+                  class="h-20 rounded w-full object-contain  object-center mb-6"
+                  src={code4alllogo}
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-xs font-medium title-font">
+                  Code your way to success
+                </h3>
+                <h2 class="text-lg  font-medium title-font mb-4">Team Code4all</h2>
+                <p class="leading-relaxed text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Debitis, mollitia.
+                </p>
+                <div className="flex gap-2 mt-4 social-links w-fit rounded-lg px-2">
+                    <h1 className="text-2xl"><a href="https://www.instagram.com/theasperteam/" target="_blank"><i class="ri-instagram-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://www.linkedin.com/company/theasperteam/mycompany/" target="_blank"><i class="ri-linkedin-fill"></i></a></h1>
+                    <h1 className="text-2xl"><a href="" target="_blank"><i class="ri-twitter-x-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://theasperteam.in/" target="_blank"><i class="ri-links-line"></i></a></h1>
+                    
+                </div>
+              </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+              <div class="club-cards  p-6">
+                <img
+                  class="h-20 rounded w-full object-contain object-center mb-6"
+                  src="https://ecellrgpv.com/assets/img/logo.png"
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-xs font-medium title-font">
+                Building An Enterprising India
+                </h3>
+                <h2 class="text-lg  font-medium title-font mb-4">Team E-Cell</h2>
+                <p class="leading-relaxed text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Debitis, mollitia.
+                </p>
+                <div className="flex gap-2 mt-4 social-links w-fit rounded-lg px-2">
+                    <h1 className="text-2xl"><a href="https://www.instagram.com/ecell_rgpv/" target="_blank"><i class="ri-instagram-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://www.linkedin.com/company/ecellrgpv/" target="_blank"><i class="ri-linkedin-fill"></i></a></h1>
+                    <h1 className="text-2xl"><a href="" target="_blank"><i class="ri-twitter-x-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://ecellrgpv.com/" target="_blank"><i class="ri-links-line"></i></a></h1>
+                    
+                </div>
+              </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+              <div class="club-cards  p-6">
+                <img
+                  class="h-20 rounded w-full object-contain object-center mb-6"
+                  src={Devoiclogo}
+                  alt="content"
+                />
+                <h3 class="tracking-widest text-xs font-medium title-font">
+                  Innovating Creativity
+                </h3>
+                <h2 class="text-lg  font-medium title-font mb-4">The Devoic</h2>
+                <p class="leading-relaxed text-base">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Debitis, mollitia.
+                </p>
+                <div className="flex gap-2 mt-4 social-links w-fit rounded-lg px-2">
+                    <h1 className="text-2xl"><a href="https://www.instagram.com/thedevoic/" target="_blank"><i class="ri-instagram-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://www.linkedin.com/company/thedevoic/" target="_blank"><i class="ri-linkedin-fill"></i></a></h1>
+                    <h1 className="text-2xl"><a href="" target="_blank"><i class="ri-twitter-x-line"></i></a></h1>
+                    <h1 className="text-2xl"><a href="https://bento.me/thedevoic" target="_blank"><i class="ri-links-line"></i></a></h1>
+                    
+                </div>
+              </div>
+            </div>
+            <div class="xl:w-1/4 md:w-1/2 p-4">
+            <div class="club-cards  p-6">
+                <img
+                  class="h-20 rounded w-full object-contain object-center mb-6"
+                  src={codingclublogo}
                   alt="content"
                 />
                 <h3 class="tracking-widest text-xs font-medium title-font">
